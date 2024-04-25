de878138ed4f276f8e6ae59a8ba1afa4f20174f4
SOSJiwon <aki_0113@naver.com>
2024-04-25 17:20:49 +0900

    개인 프로젝트 과제 제출
    구현된 기능
    1. 화면 상의 공간이동
    2. 인벤토리 확인
    3. 플레이어 정보 확인
    4. 던전 진입 (보상없음)
    
    알 수 없는 버그
    1. 인벤토리 아이템 장착에서 1번 선택 시 오류 코드로 진행됨.
    2. 상점 구매시 리스트가 이상하게 늘어나거나 줄어듬
    3. 상점 판매시 리스트가 이상하게 늘어나거나 줄어듬
    4. 선택지가 없는 번호를 골랐을 경우 오류 메세지가 원활하게 뜨지 않음

---
+++ ConsoleApp1/WriteConsoleScript.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRpgGame
+{
+   
+    // 맵 기본 정보 스크립트
+    public class DefaultScript
+    {
+        Player Player = new Player();
+
+
+        public void VillageScript()
+        {
+            Console.WriteLine(" 낡은 마을의 전경이 보입니다.");
+            Console.WriteLine(" 사람이 많은 것은 아니나, 적지도 않습니다.");
+            Console.WriteLine(" 이곳에서 던전으로 들어가기 위한 활동을 할 수 있습니다. \n");
+        }
+        public void ShopScript()
+        {
+            Console.WriteLine(" 상점 안에 들어왔습니다.");
+            Console.WriteLine(" 낡은 상점 내부는 모험가에게 필요한 물품만 판매하고 있습니다.");
+            Console.WriteLine(" 충분한 돈만 있다면 물건을 살 수 있습니다. \n");
+        }
+        public void InnScript()
+        {
+            Console.WriteLine(" 여관 안은 조용합니다.");
+            Console.WriteLine(" 단돈 500G로 하룻밤 묵을 수 있습니다. (체력 전부 회복)\n");
+        }
+        public void InventoryScript()
+        {
+            Console.WriteLine(" 자리에 앉아 장비를 점검하기 위해 가방을 열었습니다.");
+            Console.WriteLine(" 보유중인 아이템을 관리할 수 있습니다.\n");
+        }
+        public void DungeonScript()
+        {
+            Console.WriteLine(" 던전은 무서워보입니다...");
+            Console.WriteLine(" 충분한 공격력과 방어력이 있어야 클리어 할 수 있을 것 같습니다. \n");
+        }
+        public void PlayerInfoScript()
+        {
+            Console.WriteLine(" [ 플레이어 정보 ] \n");
+        }
+        public void InvenItemScript()
+        {
+            Console.WriteLine(" 아이템을 선택해서 장착하거나 장착해제할 수 있습니다.\n");
+            Console.WriteLine(" [ 아이템 관리 모드 ]\n");
+        }
+        public void ShopBuyScript()
+        {
+            Console.WriteLine(" 주인장은 아무런 관심이 없지만 당신은 양심적으로 구매하려고 합니다.\n");
+            // 타 함수로 나중에 옮기기
+            Console.WriteLine(" [ 보유골드 ]");
+            Console.WriteLine($" {Player.gold} G\n");
+        }
+        public void ShopSellScript()
+        {
+            Console.WriteLine(" 주인장은 카운터에 늘어져있던 몸을 일으키면서 말합니다.\n");
+            Console.WriteLine(" \"그래. 뭘 파시겠다고?\"");
+            Console.WriteLine(" \"잘 쳐줄테니까 한번 보여줘 봐.\"\n");
+
+            Console.WriteLine(" [ 보유골드 ]");
+            Console.WriteLine($" {Player.gold} G\n");
+
+            Console.WriteLine(" [ 아이템 판매 모드 ]\n");
+        }
+        public void InnRestScript()
+        {
+            Console.WriteLine(" 방 안에 들어가자마자 침대 위에 쓰러진 당신은 그대로 편히 잠을 잡니다.");
+            Console.WriteLine(" 아침이 밝아오는 소리에 눈을 뜹니다.");
+            Console.WriteLine(" 잘 쉬었던 것 같습니다.\n");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(" + 체력 최대치 회복\n");
+            Console.ForegroundColor = ConsoleColor.Black;
+        }
+        public void DungeonInScript()
+        {
+            Console.WriteLine(" 던전에 입장했습니다!\n");
+        }
+        public void DungeonOutScript()
+        {
+            Console.WriteLine(" 던전 클리어! \n");
+        }
+
+
+        //플레이어 정보 공개
+        public void PlayerInfoValse()
+        {
+            Console.WriteLine(" 이름 : "+Player.playerName);
+            Console.WriteLine(" 레벨 : " + Player.level);
+            Console.WriteLine(" 현재 체력 : " + Player.health +" / 100\n");
+            
+            Console.WriteLine(" 공격력 : " + Player.attack);
+            Console.WriteLine(" 방어력 : " + Player.defense + "\n");
+            
+            Console.WriteLine(" 소지금 : " + Player.gold + " G \n");
+        }
+    }
+
+    // 맵 선택지 정보 스크립트
+    public class ChoiceScript
+    {
+        string[] VillageChoice = { "플레이어 정보 (PlayerInfo)", "인벤토리 (Inventory)", "상점 (Shop)", "여관 (Inn)", "던전 (Dungeon)" };
+        string[] ShopChoice = { "상점에서 나가기 (Village)", "아이템 구매", "아이템 판매" };
+        string[] InnChoice = { "여관에서 나가기 (Village)", "휴식하기" };
+        string[] InventoryChoice = { "인벤토리 닫기 (Close)", "아이템 장착관리" };
+        string[] DungeonChoice = { "마을로 돌아가기 (Village)", "던전으로 진입" };
+        string[] PlayerInfoChoice = { "정보 그만보기 (Close)" };
+
+        string[] InvenItemChoice = { "아이템 장착관리 취소" };
+        string[] ShopBuyChoice = { "아이템 구매 취소" };
+        string[] ShopSellChoice = { "아이템 판매 취소" };
+        string[] InnRestChoice = { "마을로 돌아가기 ( Village )" };
+        string[] DungeonInChoice = { " 던전 안으로 들어간다... " };
+        string[] DungeonOutChoice = { " 던전 밖으로 나간다... " };
+
+        public void VillageScript()
+        {
+            for (int i = 0; i < VillageChoice.Length; i++)
+            {
+                Console.Write("\n " + (i + 1) + ". ");
+                Console.WriteLine($"{VillageChoice[i]}");
+            }
+        }
+        public void ShopScript()
+        {
+            for (int i = 0; i < ShopChoice.Length; i++)
+            {
+                Console.Write("\n " + (i + 1) + ". ");
+                Console.WriteLine($"{ShopChoice[i]}");
+            }
+        }
+        public void InnScript()
+        {
+            for (int i = 0; i < InnChoice.Length; i++)
+            {
+                Console.Write("\n " + (i + 1) + ". ");
+                Console.WriteLine($"{InnChoice[i]}");
+            }
+        }
+        public void InventoryScript()
+        {
+            for (int i = 0; i < InventoryChoice.Length; i++)
+            {
+                Console.Write("\n " + (i + 1) + ". ");
+                Console.WriteLine($"{InventoryChoice[i]}");
+            }
+        }
+        public void DungeonScript()
+        {
+            for (int i = 0; i < DungeonChoice.Length; i++)
+            {
+                Console.Write("\n " + (i + 1) + ". ");
+                Console.WriteLine($"{DungeonChoice[i]}");
+            }
+        }
+        public void PlayerInfoScript()
+        {
+            for (int i = 0; i < PlayerInfoChoice.Length; i++)
+            {
+                Console.Write("\n " + (i + 1) + ". ");
+                Console.WriteLine($"{PlayerInfoChoice[i]}");
+            }
+        }
+
+        public void InvenItemScript()
+        {
+            for (int i = 0; i < InvenItemChoice.Length; i++)
+            {
+                Console.Write("\n " + (i) + ". ");
+                Console.WriteLine($"{InvenItemChoice[i]}");
+            }
+        }
+        public void ShopBuyScript()
+        {
+            for (int i = 0; i < ShopBuyChoice.Length; i++)
+            {
+                Console.Write("\n " + (i) + ". ");
+                Console.WriteLine($"{ShopBuyChoice[i]}");
+            }
+        }
+        public void ShopSellScript()
+        {
+            for (int i = 0; i < ShopSellChoice.Length; i++)
+            {
+                Console.Write("\n " + (i) + ". ");
+                Console.WriteLine($"{ShopSellChoice[i]}");
+            }
+        }
+        public void InnRestScript()
+        {
+            for (int i = 0; i < InnRestChoice.Length; i++)
+            {
+                Console.Write("\n " + (i + 1) + ". ");
+                Console.WriteLine($"{InnRestChoice[i]}");
+            }
+        }
+        public void DungeonInScript()
+        {
+            for (int i = 0; i < DungeonInChoice.Length; i++)
+            {
+                Console.Write("\n " + (i + 1) + ". ");
+                Console.WriteLine($"{DungeonInChoice[i]}");
+            }
+        }
+        public void DungeonOutScript()
+        {
+            for (int i = 0; i < DungeonOutChoice.Length; i++)
+            {
+                Console.Write("\n " + (i + 1) + ". ");
+                Console.WriteLine($"{DungeonOutChoice[i]}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/item.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRpgGame
+{
+    internal class Item
+    {
+        public static List<Item> items = new List<Item>(); // 리스트 선언
+
+
+        public Item(string name, String desc, bool isHave, bool isTake, int adstat, int dpstat, int price, int idx)
+        {
+            Name = name; // 아이템 이름
+            Desc = desc; // 아이템 설명
+            IsHave = isHave; // 변동되는 변수 : 가지고 있는가?
+            IsTake = isTake;
+            ADStat = adstat;
+            DPStat = dpstat;
+            Price = price;
+            IDX = idx; // 공개 X 아이템 인덱스
+        }
+        public String Name { get; }
+        public String Desc { get; }
+        public bool IsHave { get; set; }
+        public bool IsTake { get; set; }
+        public int ADStat { get; }
+        public int DPStat { get; }
+        public int Price { get; }
+        public int IDX { get; set; }
+
+        string[] itemname = { "아이템 1", "아이템 2", "아이템 3", "아이템 4", "아이템 5", "아이템 6", "아이템 7", "아이템 8" };
+        string[] itemdesc = { "아이템 1 설명", "아이템 2 설명", "아이템 3 설명", "아이템 4 설명",
+                "아이템 5 설명", "아이템 6 설명", "아이템 7 설명", "아이템 8 설명" };
+
+        bool[] itemishave = { false, false, true, false, false, false, false, true };
+        bool[] itemistake = { false, false, true, false, false, false, false, false };
+
+        int[] itemadstat = { 10, 10, 10, 10, 10, 10, 10, 10 };
+        int[] itemdpstat = { 10, 10, 10, 10, 10, 10, 10, 10 };
+
+        int[] itemprice = { 1000, 1000, 1000, 1000, 1000, 1000, 1000, 1000 };
+        int[] itemidx = { 1, 2, 3, 4, 5, 6, 7, 8 };
+
+
+
+        public static void ItemHaveInTrue(int choice) // 아이템을 구매 할 경우 True로 바꿔주는 함수
+        {
+            foreach (Item item in items)
+            {
+                if (choice == item.IDX)
+                {
+                    switch (item.IsHave)
+                    {
+                        case false:
+                            item.IsHave = true;
+                            break;
+                        case true:
+                            item.IsHave = false;
+                            break;
+                    }
+                }
+            }
+        }
+        public static void ItemTakeInTrue(int choice) // 아이템을 착용 할 경우 True로 바꿔주는 함수
+        {
+            foreach (Item item in items)
+            {
+                if (choice == item.IDX)
+                {
+                    switch (item.IsTake)
+                    {
+                        case false:
+                            item.IsTake = true;
+                            break;
+                        case true:
+                            item.IsTake = false;
+                            break;
+                    }
+                }
+            }
+        }
+        public int AllItemInShopScript(int mapNum) // 상점 아이템 스크립트 제작
+        {
+            if (mapNum == 23) // 아이템 판매 페이지
+            {
+                AllItemInHaveScript(mapNum);
+                return 0;
+            }
+            else if (mapNum == 13) // 아이템 구매 페이지
+            {
+                int count = 1;
+                foreach (Item item in items)
+                {
+                    if (item.IsHave == false)
+                    {
+                        item.IDX = count;
+                        Console.WriteLine(" " + count + ". " + item.Name + "\t | 공격력 +" + item.ADStat + "  | 방어력 +" + item.DPStat + "  | " + item.Desc + "\t | " + item.Price + item.IDX);
+                        count++;
+                    }
+                }
+                return count-1;
+            }
+            else  // 상점 페이지
+            {
+                int count = 1;
+                foreach (Item item in items)
+                {
+                    item.IDX = count;
+                    if (item.IsHave == true)
+                        Console.WriteLine(" - " + item.Name + "\t | 공격력 +" + item.ADStat + "  | 방어력 +" + item.DPStat + "  | " + item.Desc + "\t | " + "구매완료" + item.IDX);
+                    else
+                        Console.WriteLine(" - " + item.Name + "\t | 공격력 +" + item.ADStat + "  | 방어력 +" + item.DPStat + "  | " + item.Desc + "\t | " + item.Price + item.IDX);
+                    count++;
+                }
+                return 0;
+            }
+
+
+        }
+        public void AllItemInShopList()
+        {
+            for (int i = 0; i < itemname.Length; i++)
+            {
+                items.Add(new Item(itemname[i], itemdesc[i], itemishave[i], itemistake[i], itemadstat[i], itemdpstat[i], itemprice[i], itemidx[i]));
+            }
+        } // 초기설정 : 모든 아이템 상점 리스트에 집어넣기
+        public int AllItemInHaveScript(int mapNum) //  소유한 아이템 스크립트 제작
+        {
+            if (mapNum == 2) // 인벤토리 페이지
+            {
+                int count = 1;
+                foreach (Item item in items) 
+                {
+                    item.IDX = count;
+                    // 가지고 있는 아이템만 생성
+                    if (item.IsHave == true) { Console.WriteLine(" - " + item.Name + "\t | 공격력 +" + item.ADStat + "  | 방어력 +" + item.DPStat + "  | " + item.Desc + item.IDX + item.IsHave); }
+                        else { Console.WriteLine(" 테스트 텍스트 보유하고있지않음 "+ item.Name + item.IDX); }
+                    count++;
+                }
+                return 0;
+            }
+            else if (mapNum == 12) // 인벤토리 장착 페이지
+            {
+                int count = 1;
+                foreach (Item item in items) 
+                {
+                    if (item.IsHave == true) // 가지고 있는 아이템만 나오도록
+                    {
+                        item.IDX = count;
+                        if (item.IsTake == true) // 아이템을 착용하고 있다면 E 표시
+                            Console.WriteLine(count + ". " + " [E] " + item.Name + "\t | 공격력 +" + item.ADStat + "  | 방어력 +" + item.DPStat + "  | " + item.Desc + item.IDX);
+                        else
+                            Console.WriteLine(count + ". " + "     " + item.Name + "\t | 공격력 +" + item.ADStat + "  | 방어력 +" + item.DPStat + "  | " + item.Desc + item.IDX);
+                        count++;
+                    }
+                    else { Console.WriteLine(" 테스트 텍스트 보유하고있지않음 " + item.Name + item.IDX); }
+                }
+                return count - 1;
+            }
+            else // 상점 판매 페이지
+            {
+                int count = 1;
+                foreach (Item item in items) 
+                {
+                    item.IDX = count;
+                    if (item.IsHave == true) // 가지고 있는 아이템만 나오도록
+                    {
+                        Console.WriteLine(count + ". " + item.Name + "\t | 공격력 +" + item.ADStat + "  | 방어력 +" + item.DPStat + "  | " + item.Desc + "  | " + item.Price +" G" + item.IDX + item.IsHave);
+                        count++;
+                    }
+                }
+                return count - 1;
+            }
+        }
+    }
+}
